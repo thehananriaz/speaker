6f36e1519d7d0e343b7c5239af1403a8590b9092
Hanan Riaz <thehananriaz@gmail.com>
2024-03-09 21:33:53 +0500

    typing speaker first comit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 if __name__ == '__main__':
         while True:
-            x=input("What do you wanna say: ")
+            x=input("What do you want to say: ")
             if x=="exit":
                 os.system("say 'bye'")
                 break
